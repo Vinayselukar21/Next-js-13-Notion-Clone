dd7ea4e79428cf248cd6cc8c0cd9f28cb99b0371
Vinay Selukar <vselukar2102@gmail.com>
2023-11-07 22:20:11 +0530

    Add resizable sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import React from 'react'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,8 +32,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            storageKey='jotion-theme-2'
+          >{children}</ThemeProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
   )
 }
