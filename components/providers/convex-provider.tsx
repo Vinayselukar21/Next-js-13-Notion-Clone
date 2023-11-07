9fcef87353500c3b4a87e0e1ccd26be5b6f771e4
Vinay Selukar <vselukar2102@gmail.com>
2023-11-07 22:32:06 +0530

    try-2

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -5,14 +5,14 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient("https://dutiful-mink-127.convex.cloud");
 export const ConvexClientProvider = ({
     children
 }: {
     children: ReactNode
 }) => {
     return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+        <ClerkProvider publishableKey={"pk_test_cG93ZXJmdWwtYW50ZWF0ZXItNi5jbGVyay5hY2NvdW50cy5kZXYk"}>
             <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
                 {children}
             </ConvexProviderWithClerk>
